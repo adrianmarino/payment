61f394662cdc1a36fb031df96655ad5f2f226db7
adrianmarino <adrianmarino@gmail.com>
2016-11-17 22:31:19 -0300

    rename module

--- lib/payment/rules/commercial_policy.ex
+++ lib/payment/rules/commercial_policy.ex
@@ -1,6 +1,6 @@
-defmodule CustomRule, do: defstruct name: 'Unknow', desc: 'Unknow', when: '', then: ''
+defmodule CommercialPolicy, do: defstruct name: 'Unknow', desc: 'Unknow', when: '', then: ''
 
-defimpl Rule, for: CustomRule do
+defimpl Rule, for: CommercialPolicy do
   import Utils.CodeEvaluator
   def apply?(rule, ctx), do: eval(rule.when, ctx)
   def fire(rule, ctx), do: eval(rule.then, ctx)
--- spec/payment/rule_engine_spec.exs
+++ spec/payment/rule_engine_spec.exs
@@ -5,7 +5,10 @@ defmodule RuleEngineSpec do
   describe "#fire_all" do
     context "when fire two custom rules and match one" do
       let ctx: 2
-      let rules: [%CustomRule{ when: 'ctx > 1', then: 1 }, %CustomRule{ when: 'ctx == 1', then: 2 }]
+      let rules: [
+        %CommercialPolicy{ when: 'ctx > 1', then: 1 },
+        %CommercialPolicy{ when: 'ctx == 1', then: 2 }
+      ]
       let results: described_module.fire_all rules, ctx: ctx
 
       it "returns one result", do: expect results |> to(have_length 1)
